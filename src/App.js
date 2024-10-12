a4d20db3226d2926cb5fd1f8a0c4cb8246f37ed9
Prakhar Pande <pandeprakhar1801@gmail.com>
2024-10-12 22:43:33 +0530

    App component creates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
 import React from 'react';
+import * as Components from './Components';
 
 
-export const App = () => {
+export default App = () => {
+  const[signIn,toggle] = React.useState(true);
+
   return (
-    <div>App</div>
+    <Components.Container>
+      <Components.SignUpContainer signIn = {signIn}>
+        <Components.Form>
+          <Components.Title>Create Account</Components.Title>
+          <Components.Input type='text' placeholder='Name' />
+          <Components.Input type='email' placeholder='Email' />
+          <Components.Input type='password' placeholder='Password' />
+          <Components.Button>Sign Up</Components.Button>
+        </Components.Form>
+      </Components.SignUpContainer>
+
+      <Components.SignInContainer signIn = {signIn}>
+        <Components.Form>
+          <Components.Title>
+            sign In
+          </Components.Title>
+          <Components.Input type='email' placeholder='Email' />
+          <Components.Input type='password' placeholder='Password' />
+          <Components.Anchor href='#'>Forgot Password</Components.Anchor>
+          <Components.Button>Sign In</Components.Button>
+        </Components.Form>
+      </Components.SignInContainer>
+
+      <Components.OverlayContainer signinIn={signIn}>
+        <Components.Overlay signinIn={signIn}>
+
+        <Components.LeftOverlayPanel signinIn={signIn}>
+            <Components.Title>Welcome Back!</Components.Title>
+            <Components.Paragraph>
+                To keep connected with us please login with your personal info
+            </Components.Paragraph>
+            <Components.GhostButton onClick={() => toggle(true)}>
+                Sign In
+            </Components.GhostButton>
+            </Components.LeftOverlayPanel>
+
+            <Components.RightOverlayPanel signinIn={signIn}>
+              <Components.Title>Hello, Friend!</Components.Title>
+              <Components.Paragraph>
+                  Enter Your personal details and start journey with us
+              </Components.Paragraph>
+                  <Components.GhostButton onClick={() => toggle(false)}>
+                      Sigin Up
+                  </Components.GhostButton> 
+            </Components.RightOverlayPanel>
+
+        </Components.Overlay>
+      </Components.OverlayContainer>
+
+    </Components.Container>
   )
-}
-
-
-export default App;
+};
